ab598b011e83ccd55e71c6557853eb7a523e06d5
elosine <studiotttguttt>
2019-11-01 16:03:36 -0400

    uu

--- public/javascript/sidebarmenu.js
+++ public/javascript/sidebarmenu.js
@@ -1,11 +1,10 @@
 var sidenavdiv = document.getElementById("idsidenav");
-var maindiv = document.getElementById("idmaindiv");
+var maindiv = document.getElementById("idMainContainerDiv");
 var svgNS = "http://www.w3.org/2000/svg";
 // Name ----------------------------------------------------------------- //
 var mname = document.createElement('h6');
 var mnametxt = document.createTextNode("J u s t i n  Y a n g");
 mname.id = "mname";
-// mname.style.paddingTop = "66px";
 mname.appendChild(mnametxt);
 sidenavdiv.appendChild(mname);
 // M1 ----------------------------------------------------------------- //
@@ -31,21 +30,20 @@ sidenavdiv.appendChild(link);
 var link = document.createElement('a');
 var linktxt = document.createTextNode("Contact");
 link.appendChild(linktxt);
-link.href = "#";
+link.href = "contact.html";
 sidenavdiv.appendChild(link);
 // MENU NAVICON ----------------------------------------------------------- //
 var menuicondiv = document.createElement("div");
 menuicondiv.id = "idmenuicondiv";
 menuicondiv.className = "menuicondiv";
-maindiv.appendChild(menuicondiv);
+document.body.appendChild(menuicondiv);
+// maindiv.appendChild(menuicondiv);
 var menuicondivW = document.getElementById('idmenuicondiv').offsetWidth;
 var menuicondivH = document.getElementById('idmenuicondiv').offsetHeight;
-
 var tsvgcont = document.createElementNS(svgNS, "svg");
 tsvgcont.setAttributeNS(null, "id", "idmenuiconsvgcont");
 tsvgcont.className.baseVal = "menuiconsvgcont";
 document.getElementById("idmenuicondiv").appendChild(tsvgcont);
-
 var tcirc = document.createElementNS(svgNS, "circle");
 tcirc.setAttributeNS(null, "id", "menuiconcirc");
 tcirc.setAttributeNS(null, "cx", (menuicondivW / 2));
@@ -56,7 +54,6 @@ tcirc.setAttributeNS(null, "fill", "#2BAE66FF");
 tcirc.className.baseVal = "menucirc";
 tcirc.addEventListener("click", openNav);
 document.getElementById("idmenuiconsvgcont").appendChild(tcirc);
-
 var linetopoffset = 16;
 var linesideoffset = 12;
 var circctr = menuicondivH / 2;
@@ -97,13 +94,11 @@ document.getElementById("idmenuiconsvgcont").appendChild(btmline);
 // CLOSE/OPEN DIV FUNCTION ------------------------------------------- //
 var sidenavW = "145px";
 var navopen = false;
-
 function openNav() {
-
   if (!navopen) {
-    document.getElementById("idsidenav").style.width = sidenavW;
-    document.getElementById("idmaindiv").style.marginLeft = sidenavW;
-
+    // Open side menu by changing its width
+    sidenavdiv.style.width = sidenavW;
+    //Menu Icon Animation
     tcirc.setAttributeNS(null, "fill", "#FAD0C9FF");
     topline.setAttributeNS(null, "stroke-width", (circctr - linesideoffset - 3));
     midline.setAttributeNS(null, "stroke-width", (circctr - linesideoffset - 3));
@@ -115,13 +110,8 @@ function openNav() {
     topline.setAttributeNS(null, "stroke", "black");
     btmline.setAttributeNS(null, "stroke", "black");
     navopen = true;
-    if (!window.matchMedia('screen and (max-width: 700px)').matches) {
-      document.getElementById("idmaindiv").style.width = "calc(100vw - " + sidenavW + ")";
-    }
-
   } else {
     document.getElementById("idsidenav").style.width = "0";
-    document.getElementById("idmaindiv").style.marginLeft = "0";
     topline.setAttributeNS(null, "stroke-width", lineW);
     midline.setAttributeNS(null, "stroke-width", lineW);
     btmline.setAttributeNS(null, "stroke-width", lineW);
@@ -133,9 +123,6 @@ function openNav() {
     btmline.setAttributeNS(null, "stroke", "black");
     tcirc.setAttributeNS(null, "fill", "#2BAE66FF");
     navopen = false;
-    if (!window.matchMedia('screen and (max-width: 700px)').matches) {
-      document.getElementById("idmaindiv").style.width = "calc(100vw)";
-    }
   }
 }
 // Get rid of hover events on touchscreens ////////////////////////
@@ -144,16 +131,13 @@ function hasTouch() {
     navigator.maxTouchPoints > 0 ||
     navigator.msMaxTouchPoints > 0;
 }
-
 if (hasTouch()) { // remove all :hover stylesheets
   try { // prevent exception on browsers not supporting DOM styleSheets properly
     for (var si in document.styleSheets) {
       var styleSheet = document.styleSheets[si];
       if (!styleSheet.rules) continue;
-
       for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
         if (!styleSheet.rules[ri].selectorText) continue;
-
         if (styleSheet.rules[ri].selectorText.match(':hover')) {
           styleSheet.deleteRule(ri);
         }
@@ -166,21 +150,6 @@ window.addEventListener('resize', winResize, false);
 
 function winResize() {
   if (window.matchMedia('screen and (max-width: 768px)').matches) {
-    console.log("too big");
+
   }
-
 }
-
-
-// blendgroup.setAttribute('style', "isolation: isolate");
-// var myname = document.createElement('h6');
-// var nametxt = document.createTextNode("Justin Yang");
-// myname.appendChild(nametxt);
-// sidenavdiv.appendChild(myname);
-//
-// //resize main body div
-// var mainbody = document.getElementById("main");
-// var head = document.getElementById("head1");
-// var headheight = head.offsetHeight;
-// headheight = headheight + 55;
-// mainbody.setAttribute( "style", "height:calc(100vh - " + headheight + "px)" );
